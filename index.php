7500f4cfee4e8b2dff1d3063c8a68af0406b4e9d
hk-p <hukada.gogotea@gmail.com>
2021-02-24 23:55:54 +0900

    merge

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="style.css" />
+		<title>shooting2</title>
+	</head>
+	<body>
+		<canvas id="canvas"></canvas>
+		<script type="text/javascript" src="functions.js"></script>
+		<script type="text/javascript" src="Star.js"></script>
+		<script type="text/javascript" src="Sprite.js"></script>
+		<script type="text/javascript" src="Character.js"></script>
+		<script type="text/javascript" src="Explosion.js"></script>
+		<script type="text/javascript" src="Bullet.js"></script>
+		<script type="text/javascript" src="Enemy.js"></script>
+		<script type="text/javascript" src="EnemyShot.js"></script>
+		<script type="text/javascript" src="Player.js"></script>
+		<script type="text/javascript" src="spritesInit.js"></script>
+		<script type="text/javascript" src="main.js"></script>
+	</body>
+</html>
